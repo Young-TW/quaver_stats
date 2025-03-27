3984ba5219259e32f0da2ff30f5795c05c52153b
Young <young20050727@gmail.com>
2025-03-27 15:53:39 +0800

    feat: print all user stats

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use Quaver_Stats::user::User;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut player: User = User::new();
     player.set_name("tyrcs".to_string());
+
+    let id = User::fetch_id(&player.name).await.unwrap();
+    let fetched = User::fetch_stat(id).await.unwrap();
+
+    println!("{:#?}", fetched);
 }
